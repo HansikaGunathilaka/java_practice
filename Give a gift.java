1aa518ee5510a48bf06868aeacebeb06b3507902
HansikaGunathilaka <85073973+HansikaGunathilaka@users.noreply.github.com>
2021-06-20 22:15:00 +0530

    First Commit

---
+++ Give a gift.java
@@ -0,0 +1,13 @@
+class PassOrFail {
+	public static void main(Sting [] args){
+		int science_mark;
+		science_mark=75;
+		if(science_mark>40) {
+			system.out.println("Pass");
+			system.out.println("Give a gift");
+		}
+		else
+		{system.out.println("Fail");
+		}
+	}
+}
